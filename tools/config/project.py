485a8cd51548626f7de9215d45963ff97147affc
TakWolf <takwolf@foxmail.com>
2026-09-25 18:47:42 +0800

    基于基础拉丁字母的构建骨架

---
+++ tools/config/project.py
@@ -0,0 +1,18 @@
+
+VERSION = '2026.01.01'
+
+FONT_BRAND = 'Chaos Pixel'
+FONT_LANGUAGE = 'Arabic'
+FILE_NAME_PREFIX = 'chaos-pixel'
+
+DESCRIPTION = 'Open-source Arabic pixel font in a sans-serif style'
+
+DESIGNER = 'TakWolf'
+DESIGNER_URL = 'https://takwolf.com'
+
+MANUFACTURER = 'TakWolf'
+VENDOR_URL = 'https://chaos-pixel-font-arabic.takwolf.com'
+
+COPYRIGHT_INFO = 'Copyright (c) 2026, TakWolf (https://takwolf.com)'
+LICENSE_INFO = 'This Font Software is licensed under the SIL Open Font License, Version 1.1'
+LICENSE_URL = 'https://github.com/TakWolf/chaos-pixel-font-arabic/blob/master/LICENSE-OFL'
